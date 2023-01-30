765a3064e037558ec6c2a53102147e4ec93a0716
Mirella <mirella.dimichino@gmail.com>
2023-01-30 16:43:26 +0000

    added btn search and cityval/ uncommented bootstrap links

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 var APIKey = "0cfea648363320704e44344ecbc38810";
 
-// Here we are building the URL we need to query the database
 
-var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=London&appid=" + APIKey;
+//creating a button search
+$("#search-button").on("click", function (event) {
+    
+    event.preventDefault();// preventing page refresh
+
+    cityVal = $("#search-input").val();// cretaed a cityvalue
+    
+    searchBtn = $("<button>").text(cityVal)// button with city text
+    
+    $(".list-group").append(searchBtn)// appending city lists
+
+    if (!cityVal) {
+      alert("You must chose a city name!")
+    }
+    
+// url for the weather API
+var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityVal + "&appid=" + APIKey;
 
 // I  created an AJAX call
 $.ajax({
@@ -11,15 +26,7 @@ $.ajax({
 }).then(function(response) {
   console.log(queryURL);
   console.log(response);
-  var celsiusTemperature = Math.floor(response.main.temp - 273.15);
-  // Create CODE HERE to transfer content to HTML
-  var myTempDiv = $('.temp');
-  myTempDiv.text(celsiusTemperature + " degrees Celsius");
-  var city = response.name;
-  var humidity = response.main.humidity;
-  var wind = response.wind.speed;
-  $('.city').text("Weather for " + city);
-  $('.humidity').text("Relative humidity: " + humidity + "%");
-  $('.wind').text("Wind speed: " + wind + " m/s");
-    
+ 
 })
+})
+
